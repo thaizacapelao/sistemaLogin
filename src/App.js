5e6292bcbcba53b2ed23f3bd394585cc8a1d358f
Thaiza Capelão <thaizacapelao@gmail.com>
2023-02-03 15:14:21 -0400

    Co-authored-by: Thaiza Capelão <thaizacapelao@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import RoutesApp from "./routes";
+import { AuthProvider } from "./contexts/auth";
+import GlobalStyle from "./styles/global";
+
+const App = () => (
+  <AuthProvider>
+    <RoutesApp />
+    <GlobalStyle />
+  </AuthProvider>
+);
+
+export default App;
